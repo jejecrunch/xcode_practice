22f7c8adde2b32589ee5b0d0acf21c0499af4b05
jejecrunch <user@yujeoui-MacBook-Pro.local>
2021-01-29 09:43:52 +0900

    210129 Timer 이용해서 계속 현재시간 업데이트 되는 구현 실습

--- DatePicker/DatePicker/ViewController.swift
+++ DatePicker/DatePicker/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let timeSelect: Selector = #selector(ViewController.updateTime)
+    let interval = 1.0
+    var count = 0
+    
 
     @IBOutlet var lblCurrentTime: UILabel!
     @IBOutlet var lblPickerTime: UILabel!
@@ -16,6 +20,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelect, userInfo: nil, repeats: true)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,6 +35,17 @@ class ViewController: UIViewController {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
         lblPickerTime.text = "select time: "+formatter.string(from: datePickerView.date)
+    }
+    
+    @objc func updateTime() {
+//        lblCurrentTime.text = String(count)
+//        count = count + 1
+        
+        let date = NSDate()
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        lblCurrentTime.text = "current time: " + formatter.string(from: date as Date)
     }
 
 }
