100a49d7a2237d41c3f8c0986fb3eabd85ecee11
jejecrunch <user@yujeoui-MacBook-Pro.local>
2021-02-01 23:23:45 +0900

    210201 PickerView에서 피커 뷰 룰렛에 파일명 대신 이미지 출력하기 실습

--- PickerView/PickerView/ViewController.swift
+++ PickerView/PickerView/ViewController.swift
@@ -46,8 +46,15 @@ class ViewController: UIViewController, UIPickerViewDelegate {
         return imageFileName.count
     }
     
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return imageFileName[row]
+//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//        return imageFileName[row]
+//    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let imageView = UIImageView(image:imageArray[row])
+        imageView.frame = CGRect.init(x:0, y:0, width:100, height:150)
+        
+        return imageView
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
