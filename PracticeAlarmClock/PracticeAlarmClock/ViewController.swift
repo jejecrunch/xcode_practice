d777f5fc12a400a10359e5262ec9973dcac21a9f
jejecrunch <user@yujeoui-MacBook-Pro.local>
2021-02-04 22:18:51 +0900

    210204 Alert 기능이 포함된 Alarm 구현 실습

--- PracticeAlarmClock/PracticeAlarmClock/ViewController.swift
+++ PracticeAlarmClock/PracticeAlarmClock/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     let timeSelector: Selector = #selector(ViewController.updateTime)
     // 타이머가 구동되면 실행하는 함수
     let minSelector : Selector = #selector(ViewController.minTime)
+    // 타이머를 킬 것인지 끌 것인지
+    var isTimerState = false
     // 타이머 간격
     let interval = 1.0
     var count = 0
@@ -56,9 +58,38 @@ class ViewController: UIViewController {
 
         lblCurrentTime.text = "current time: "+formatter.string(from: date as Date)
         
+        
         if picTime == curTime {
-            Timer.scheduledTimer(timeInterval: interval, target: self, selector: minSelector, userInfo: nil, repeats: true)
+            isTimerState = true
+            
+            let alert = UIAlertController(title: "알림", message: "설정된 시간입니다 !!!", preferredStyle: UIAlertControllerStyle.alert)
+            
+            let onAction = UIAlertAction(title: "네, 알겠습니다.", style: .default, handler: {
+                ACTION in
+                // 60초 후에 알림을 다시 켜주는 타이머
+                Timer.scheduledTimer(timeInterval: 60.0, target: self, selector: #selector(self.timerOn), userInfo: nil, repeats: true)
+            })
+            
+            alert.addAction(onAction)
+            present(alert, animated: true, completion: nil)
         }
+    }
+    
+    @objc func timerOn(){
+        isTimerState = false
+        // 실제로 켜지는지 확인하려고 넣은 코드
+        print("알람 기능이 켜졌습니다")
+    }
+    
+    // 1분동안 알림이 안열릴 수 있게
+    // 아예 얼럿 기능이 진행되지 않게는 못하는 코드
+    // 얼럿이 켜지면 바로 무시만 되는 ...
+    @objc func noAlert(){
+        count+=1
+        if count < 60 {
+            dismiss(animated: true, completion: nil)
+        }
+        print(count)
     }
     
     // 1분마다 바뀔 수 있게
