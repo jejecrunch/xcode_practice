50d051bf14cd596280c72491f16b52d40cccff07
jejecrunch <user@yujeoui-MacBook-Pro.local>
2021-02-10 14:58:43 +0900

    210210 갤러리 앱에 스와이프 기능 추가하기 구현 실습 - 역시 보강할 것

--- PageControl/PageControl/ViewController.swift
+++ PageControl/PageControl/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 var images = [ "01.png", "02.png", "03.png"]
 
 class ViewController: UIViewController {
+    
+    let numOfTouchs = 2
 
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var pageControl: UIPageControl!
@@ -21,9 +23,21 @@ class ViewController: UIViewController {
         
         pageControl.numberOfPages = images.count
         pageControl.currentPage = 0
+        
         pageControl.pageIndicatorTintColor = UIColor.green
         pageControl.currentPageIndicatorTintColor = UIColor.red
+        
         imgView.image = UIImage(named: images[0])
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
+        swipeLeft.direction = .left
+        swipeLeft.numberOfTouchesRequired = numOfTouchs
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
+        swipeRight.direction = .right
+        swipeRight.numberOfTouchesRequired = numOfTouchs
+        self.view.addGestureRecognizer(swipeRight)
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,6 +47,25 @@ class ViewController: UIViewController {
 
     @IBAction func pageChanged(_ sender: UIPageControl) {
         imgView.image = UIImage(named: images[pageControl.currentPage])
+    }
+    
+    @objc func respondToSwipeGesture(_ gesture: UIGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            
+            if swipeGesture.direction == .left {
+                if (pageControl.currentPage<pageControl.numberOfPages-1) {
+                    pageControl.currentPage = pageControl.currentPage + 1
+                }
+
+            } else if swipeGesture.direction == .right {
+                if (pageControl.currentPage>0) {
+                    pageControl.currentPage = pageControl.currentPage - 1
+                }
+            }
+            
+            imgView.image = UIImage(named: images[pageControl.currentPage])
+            
+        }
     }
 
 }
